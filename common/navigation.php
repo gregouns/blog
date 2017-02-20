1bcb5fe87935ac138ee862864e706c8a29fb03b1
gregouns <greglelay@gmail.com>
2017-02-20 19:01:00 +0100

    navigation.php

--- common/navigation.php
+++ common/navigation.php
@@ -8,13 +8,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">accueil</a>
+      <a class="navbar-brand" href="/blog/blog.git/accueil">accueil</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="/post">post <span class="sr-only">(current)</span></a></li>
+        <li class="active"><a href="/blog/blog.git/post">post <span class="sr-only">(current)</span></a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->

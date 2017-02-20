da25cd4c2969d98669008f80215733d4417b47f0
gregouns <greglelay@gmail.com>
2017-02-21 00:50:57 +0100

    add posts

--- page/home.php
+++ page/home.php
@@ -1,1 +1,15 @@
-dfgdfgdfgdfgdfg
+<?php
+	setlocale(LC_ALL, 'fr_FR');
+	setlocale(LC_TIME, "fr_FR");
+	setlocale (LC_TIME, 'fr_FR.utf8','fra'); 
+
+	$query = "SELECT *, UNIX_TIMESTAMP(date) AS timestamp FROM posts WHERE status = 1 ORDER BY date DESC LIMIT 10";
+	$rst = mysqli_query($cnt, $query);
+
+	while($arr = mysqli_fetch_array($rst)) {
+		// echo '<a href="/post/'.slugify($arr['title']).'">' . $arr['title'] . '</a><br />';
+		echo '<a href="/post/'.$arr['id'].'">' . $arr['title'] . '</a><br />';
+		echo date('D j M Y', $arr['timestamp']);
+		echo '<br /><br />';
+	}
+?>

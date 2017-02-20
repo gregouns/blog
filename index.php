054fb97fc473d6f0431da91f43a8474ffcae8d7e
gregouns <greglelay@gmail.com>
2017-02-20 17:59:13 +0100

    index.php

--- index.php
+++ index.php
@@ -5,28 +5,25 @@ error_reporting(E_ALL);
 if (isset($_GET['page'])) {
 	switch ($_GET['page']) {
 		case 'accueil':
-			$page = './pages/home.php';
+			$page  = './pages/home.php';
 			$title = "home";
 			break;
-
 		case 'post':
-			$page = './pages/post.php';
+			$page  = './pages/post.php';
+			$title = "post";
 			break;
-
 		default:
 			$title = "Erreur";
 			$page  = './pages/erreur404.php';
 			break;
-
 	}
 }
 else { 
-	$page = './pages/home.php';
+	$page  = './pages/home.php';
 	$title = "home";
-
 }
-	include ('./common/header.php');
-	include ('./common/navigation.php');
-	include ($page);
-	include ('./common/footer.php');
+include ('./common/header.php');
+include ('./common/navigation.php');
+include ($page);
+include ('./common/footer.php');
 	

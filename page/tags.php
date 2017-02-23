15c93bc95db9c8600cbaa15d087c0db4ec372c5f
gregouns <greglelay@gmail.com>
2017-02-23 16:13:48 +0100

    modif

--- page/tags.php
+++ page/tags.php
@@ -1,10 +1,11 @@
 <?php
 	$tags = $_GET['tag'];
-	$query = "SELECT tags.* tags WHERE id = '{$tag}' LIMIT 1";
-
-	$rst = mysqli_query($cnt, $query);
+	$query = "SELECT post.* FROM post JOIN post_tags ON post.post_id = post_tag.post_id JOIN tag ON tags.tag_id = post_tag.tag_id where status = 1";
+	$strt = mysqli_query($cnt, $query);
 
 	while($arr = mysqli_fetch_array($rst)) {
-		echo '<a href="/tags/'.$arr['id_post'].'">' . $arr['tags'] . '</a><br />';
+		echo '<a href="/tags/'.$arr['tag_id'].'">' . $arr['tag'] . '</a><br />';
+		echo '<a href="/post/'.$arr['post_id'].'">' . $arr['title'] . '</a><br />';
+		echo $arr['description'] . '<br />';
+		echo date('D j M Y', $arr['timestamp']);
 		echo '<br /><br />';
-	}

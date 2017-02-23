4b3e18df345e773bc0e6b612f190731c412c3725
gregouns <greglelay@gmail.com>
2017-02-23 16:29:43 +0100

    indentation

--- page/home.php
+++ page/home.php
@@ -5,14 +5,14 @@
 
 	// $posts = array('id' ,'title','date','description','status');
 	// $tags  =  array('id','id_post','tag','status');
-		$query = "SELECT tag_id,tag, post_id, title, description, UNIX_TIMESTAMP(date) AS timestamp FROM tags, posts WHERE tag_id = post_id DESC LIMIT 10";
+		$query = "SELECT tag_id,tag, post_id, title, description, UNIX_TIMESTAMP(date) AS timestamp FROM tags, posts WHERE tag_id = post_id ORDER BY date DESC LIMIT 10";
 		$rst = mysqli_query($cnt, $query);
 
 		while($arr = mysqli_fetch_array($rst)) {
 			// echo '<a href="/post/'.slugify($arr['title']).'">' . $arr['title'] . '</a><br />';
-			echo 'Title: <a style="color:orange;" href="/post/'.$arr['post_id'].'">' . $arr['title'] . '</a><br />';
+			echo 'Title: <a style="color:orange;" href="/post/'.slugify($arr['post_id']).'">' . $arr['title'] . '</a><br />';
 			echo 'Date: ' . date('D j M Y', $arr['timestamp']) . '<br/> Description: ' . $arr['description'] . '<br/>';
-			echo 'Tag: <a href="/tags/'.$arr['tag_id'].'">' . $arr['tag'] . '</a><br />';
+			echo 'Tag: <a href="/tags/'.slugify($arr["tag_id"]).'">' . $arr['tag'] . '</a><br />';
 			echo '<br/><br/>';
 		}
 

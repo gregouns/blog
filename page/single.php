01067bb3f0205f2cc0dd54df34748d84dbb7b0d6
gregouns <greglelay@gmail.com>
2017-02-23 16:14:12 +0100

    modif couleur texte

--- page/single.php
+++ page/single.php
@@ -2,13 +2,13 @@
 
 	$post = $_GET['post'];
 
-	$query = "SELECT *, UNIX_TIMESTAMP(date) AS timestamp FROM posts WHERE status = 1 AND id = '{$post}' LIMIT 1";
+	$query = "SELECT *, UNIX_TIMESTAMP(date) AS timestamp FROM posts WHERE status = 1 AND post_id = '{$post}' LIMIT 1";
 	$rst = mysqli_query($cnt, $query);
 
 	while($arr = mysqli_fetch_array($rst)) {
-		echo '<a href="/post/'.$arr['id'].'">' . $arr['title'] . '</a><br />';
-		echo $arr['description'] . '<br />';
-		echo date('D j M Y', $arr['timestamp']);
+		echo 'Title: <a style="color:orange;" href="/post/'.$arr['post_id'].'">' . $arr['title'] . '</a><br />';
+		echo 'Description: ' . $arr['description'] . '<br />';
+		echo 'Date: ' . date('D j M Y', $arr['timestamp']);
 		echo '<br /><br />';
 	}
 	

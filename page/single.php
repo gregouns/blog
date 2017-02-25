a9f6141ec0b9f08648db45905705f8c750f94846
gregouns <greglelay@gmail.com>
2017-02-25 13:55:41 +0100

    affichage de post et supression des tags

--- page/single.php
+++ page/single.php
@@ -2,11 +2,11 @@
 
 	$post = $_GET['post'];
 
-	$query = "SELECT *, UNIX_TIMESTAMP(date) AS timestamp FROM posts WHERE status = 1 AND post_id = '{$post}' LIMIT 1";
+	$query = "SELECT *, UNIX_TIMESTAMP(date) AS timestamp FROM posts WHERE status = 1 AND id = '{$post}' LIMIT 1";
 	$rst = mysqli_query($cnt, $query);
 
 	while($arr = mysqli_fetch_array($rst)) {
-		echo 'Title: <a style="color:orange;" href="/post/'.$arr['post_id'].'">' . $arr['title'] . '</a><br />';
+		echo 'Title: <a style="color:orange;" href="/post/'.$arr['id'].'">' . $arr['title'] . '</a><br />';
 		echo 'Description: ' . $arr['description'] . '<br />';
 		echo 'Date: ' . date('D j M Y', $arr['timestamp']);
 		echo '<br /><br />';

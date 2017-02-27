1f9e0dcd68d7ac1cd6d669e85cb62bed2ebf6f3a
gregouns <greglelay@gmail.com>
2017-02-27 21:43:46 +0100

    modification to have post and tags together

--- page/single.php
+++ page/single.php
@@ -4,13 +4,18 @@
 
 	$post = $_GET['post'];
 
-	$query = "SELECT *, UNIX_TIMESTAMP(date) AS timestamp FROM posts WHERE id = '{$post}' AND status = 1 LIMIT 1";
+	$query = "SELECT *, UNIX_TIMESTAMP(date) AS timestamp FROM posts WHERE url = '{$post}' AND status = 1 LIMIT 1";
 	$rst = mysqli_query($cnt, $query);
 		while($arr = mysqli_fetch_array($rst)) {
 			echo 'Title: <a style="color:orange;" href="/post/'.$arr['id'].'">' . $arr['title'] . '</a><br />';
 			echo 'Description: ' . $arr['description'] . '<br />';
 			echo 'Date: ' . date('D j M Y', $arr['timestamp']);
-			echo '<br /><br />';
+			echo '<br />';
+			$query2 = "SELECT * FROM tags AS t,posts_tags AS pt WHERE pt.post_id = '{$arr['id']}'AND pt.tag_id = t.id";
+			$rst2 = mysqli_query($cnt2,$query2);
+			while($arr2 = mysqli_fetch_array($rst2)) {
+				echo 'tag: <a href = "/tags/'.$arr2['url'].'">' . $arr2['tag'] . '</a><br />';
+			}
 		}
 
 ?>

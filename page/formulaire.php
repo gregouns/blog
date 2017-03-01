c627c546350dca2f850821d9a2a81e790baac09a
gregouns <greglelay@gmail.com>
2017-03-01 13:51:31 +0100

    formulaire with arborescence

--- page/formulaire.php
+++ page/formulaire.php
@@ -5,9 +5,6 @@ $cat     = '';
 if (isset($_POST['cat'])) {
 	$cat  = $_POST['cat'];
 	$arr_cat = array();
-	if ($cat != '') {
-		$arr_cat = explode(',', $cat);
-	}
 }
 
 $Scat = '';
@@ -21,6 +18,7 @@ if (isset($_POST['Scat']))	{
 
 
 
+
 if ( $_POST ) {
 	$_error = false;
 	if ( isset($_POST['cat']) && strlen($cat) == 0 ) {
@@ -30,33 +28,66 @@ if ( $_POST ) {
 	else {
 		$cat   = strip_tags($cat);
 		$Scat  = strip_tags($Scat);
-		// condition categorie exist
+		// condition si categorie du premier input exist
 		$query_cat_exist = "SELECT * FROM categories Where name = '{$cat}'";
 		$rst_cat_exist = mysqli_query($cnt,$query_cat_exist);
 		if(mysqli_num_rows($rst_cat_exist) > 0) {
-			$query_cat_recup = "SELECT cat.id AS cid FROM categories Where name = '{$name}'";
-			$rst_cat_recup = mysqli_query($cnt, $query_cat_recup);
-			foreach ($arr_Scat as $key => $Scat) {
-				while($arr = mysqli_fetch_array($rst_cat_recup)) {
-					$query_Scat_insert = "INSERT INTO categories (`id`, `id_parent`, `name`, `status`) 
-						VALUES (NULL, '{$arr['id']}' , `{$Scat}` , 1)";
-						echo $query_Scat_insert;
-					$rstScat = mysqli_query($cnt, $query_Scat_insert);
+			// recupere l' id de la categorie existante
+			$query_Scat_exist = "SELECT * FROM categories Where name = '{$Scat}'";
+			$rst_Scat_exist = mysqli_query($cnt,$query_Scat_exist);
+			if(mysqli_num_rows($rst_Scat_exist) > 0) {
+				// condition si la sous categorie existe deja et je regarde si je ne peux pas l a update sous un nouveau menu
+				$query_cat_update = "SELECT * FROM categories Where name = '{$cat}'";
+				$rst_cat_update = mysqli_query($cnt,$query_cat_update);
+				if ($arr = mysqli_fetch_array($rst_cat_update)) {
+					$query_Scat_update = "UPDATE categories SET  id_parent = '{$arr['id']}' WHERE name = '{$Scat}'";
+					$rst_Scat_update = mysqli_query($cnt,$query_Scat_update);
+					$message = '<div class="alert alert-success">Votre menu a bien été inséré</div>';
+					echo $message;
+					echo 'update';
+				}
+			}	
+			if ( isset($Scat) && strlen($Scat) == 0 ) {
+				// condition si la personne n a pas choisi de sous menu
+				$message = '<div class="alert alert-success">Votre menu a bien été inséré</div>';
+				echo $message;
+			}
+			else { 
+				$query_Scat_exist = "SELECT * FROM categories Where name = '{$Scat}'";
+				$rst_Scat_exist = mysqli_query($cnt,$query_Scat_exist);
+				if(mysqli_num_rows($rst_Scat_exist) == 0) {
+					// insertion du nouveau sous menu
+					$query_cat_recup = "SELECT id FROM categories Where name = '{$cat}'";
+					$rst__cat_recup = mysqli_query($cnt, $query_cat_recup);
+					while ($arr = mysqli_fetch_array($rst__cat_recup)) {
+						$query_Scat_insert = "INSERT INTO categories (id,id_parent,name,status) 
+							VALUES (NULL,'{$arr['id']}','{$Scat}',1)";
+						$rst_Scat = mysqli_query($cnt, $query_Scat_insert);
+						echo 'ajout d un nouveau sous menu';
+					}
 				}
 			}	
 		}
+		// condition 1er menu n' existe pas
 		else {
-			$query_cat_insert = "INSERT INTO categories (`id`, `id_parent`, `name`, `status`) VALUES ( NULL, NULL , `{$_POST['cat']}` , 1)";
-			echo $query_cat_insert;
+			// insertion du menu
+			$query_cat_insert = "INSERT INTO `categories` (`id`, `id_parent`, `name`, `status`) VALUES ( NULL,0,'{$_POST['cat']}','1')";
 			$rst_cat_insert = mysqli_query($cnt, $query_cat_insert);
-			$id = mysqli_insert_id($cnt);
-			foreach ($arr_Scat as $key => $Scat) {
-				$query_Scat_insert = "INSERT INTO categories (`id`, `id_parent`, `name`, `status`) 
-					VALUES (NULL, '{$id}' , `{$Scat}` , 1)";
-				$rstScat = mysqli_query($cnt, $query_Scat_insert);
+			if ( isset($Scat) && strlen($Scat) == 0 ) {
+				$message = '<div class="alert alert-success">Votre menu a bien été inséré</div>';
+				echo $message;
 			}
-			$message = '<div class="alert alert-success">Votre post a bien été inséré</div>';
-		}		
+			else {
+				// recherche id du parent crée
+				$query_cat_select = "SELECT id FROM categories Where name ='{$cat}'";
+				$rst_cat_select = mysqli_query($cnt,$query_cat_select);
+				while ($arr = mysqli_fetch_array($rst_cat_select)) {
+				$query_Scat_insert = "INSERT INTO categories (id,id_parent,name,status) 
+					VALUES (NULL,'{$arr['id']}','{$Scat}',1)";
+				$rst_Scat = mysqli_query($cnt, $query_Scat_insert);
+				}
+			}
+		}	
 	}
 }
 

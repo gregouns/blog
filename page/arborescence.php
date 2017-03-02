5040895a5db0fc71d772d826623718fd37c4cdc3
gregouns <greglelay@gmail.com>
2017-03-02 15:00:46 +0100

    add select and option

--- page/arborescence.php
+++ page/arborescence.php
@@ -14,26 +14,18 @@ while ($arr = mysqli_fetch_array($rst)) {
 		'category'  => $arr['name']
 	);
 }
-echo '<h2>choose your categories</h2>';
 function afficher_menu($id_parent, $niveau, $array) {
- 
-$html = "";
- 
-foreach ($array AS $lien) {
- 
-	if ($id_parent == $lien['id_parent']) {
- 
-	for ($i = 0; $i < $niveau; $i++) $html .= "--";
- 
-	$html .= '<label><select id="cat'.$lien['id'].'"><option>' . $lien['category'] . '<option/></select></label><br />';
- 
-	$html .= afficher_menu($lien['id'], ($niveau + 1), $array);
- 
-	}
- 
-}
- 
-return $html;
- 
+	$html = "";
+	foreach ($array AS $lien) {
+		if ($id_parent == $lien['id_parent']) {
+		 
+			for ($i = 0; $i < $niveau; $i++) {
+				$html .= "----";
+			}
+			$html .= '<option id ="'.$lien['id'].'">' . $lien['category'] . '<option/>';
+			$html .= afficher_menu($lien['id'], ($niveau + 1), $array);
+		}
+	}	 
+	return $html; 
 }
 echo afficher_menu(0, 0, $cat);

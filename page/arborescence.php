565664ae3cb74ce75d0d1c3df36812c0d5a08c36
gregouns <greglelay@gmail.com>
2017-03-03 09:06:05 +0100

    deleted space

--- page/arborescence.php
+++ page/arborescence.php
@@ -14,18 +14,14 @@ while ($arr = mysqli_fetch_array($rst)) {
 		'category'  => $arr['name']
 	);
 }
-function afficher_menu($id_parent, $niveau, $array) {
+function afficher_menu($id_parent, $array) {
 	$html = "";
 	foreach ($array AS $lien) {
 		if ($id_parent == $lien['id_parent']) {
-		 
-			for ($i = 0; $i < $niveau; $i++) {
-				$html .= "----";
-			}
-			$html .= '<option id ="'.$lien['id'].'">' . $lien['category'] . '<option/>';
-			$html .= afficher_menu($lien['id'], ($niveau + 1), $array);
+			$html .= '<option id ="'.$lien['id'].'">' . $lien['id_parent'] . ' ' . $lien['category'] . '</option>';
+			$html .= afficher_menu($lien['id'], $array);
 		}
 	}	 
 	return $html; 
 }
-echo afficher_menu(0, 0, $cat);
+echo afficher_menu(0, $cat);

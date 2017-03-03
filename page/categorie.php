6c0735a12ae235d5db3799648f89e6dcf2a61517
gregouns <greglelay@gmail.com>
2017-03-03 16:57:01 +0100

    page finished

--- page/categorie.php
+++ page/categorie.php
@@ -1,21 +1,22 @@
 <?php
-$cat = $_GET['catego'];
+$cat = $_GET['categorie'];
+
 
 	// I want the post that has catgerory
-	$query = "SELECT * 
-		FROM 
-			posts AS p, 
-			posts_tags AS pt, 
-			categorie AS c 
-		WHERE
-			p.id = pt.post_id
-			AND pt.cat_id = c.id
-			AND c.name = '{$cat}'
-	";
-	$rst = mysqli_query($cnt,$query);
-	while($arr = mysqli_fetch_array($rst)) {
-		echo 'categorie: <a href = "/categorie/'.$arr['name'].'">' . $arr['name'] . '</a><br/>';
-		echo 'post: <a href = "/post/'.$arr['title'].'">' . $arr['title'] . '</a><br/>';
-		echo 'description: ' . $arr['description'];
-		echo 'date: ' . $arr['date'] . '<br/>';
-	}
+$query = "SELECT * , p.url AS purl 
+	FROM 
+		posts AS p, 
+		posts_tags AS pt, 
+		categories AS c 
+	WHERE
+		p.id = pt.post_id
+		AND pt.cat_id = c.id
+		AND c.name = '{$cat}'
+";
+$rst = mysqli_query($cnt,$query);
+while($arr = mysqli_fetch_array($rst)) {
+	echo 'categorie: <a style="color:green;" href = "/categorie/'.$arr['url'].'">' . $arr['name'] . '</a><br/>';
+	echo 'post: <a style="color:orange;" href = "/post/'.$arr['purl'].'">' . $arr['title'] . '</a><br/>';
+	echo 'description: ' . $arr['description'] . '<br/>';
+	echo 'date: ' . $arr['date'] . '<br/>';
+}

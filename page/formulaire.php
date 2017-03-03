e5a1bce013908df1c301e993b450b25f48ac905b
gregouns <greglelay@gmail.com>
2017-03-03 13:21:03 +0100

    add post categorie

--- page/formulaire.php
+++ page/formulaire.php
@@ -61,8 +61,8 @@ if ( $_POST ) {
 					$query_cat_recup = "SELECT id FROM categories Where name = '{$cat}'";
 					$rst__cat_recup = mysqli_query($cnt, $query_cat_recup);
 					while ($arr = mysqli_fetch_array($rst__cat_recup)) {
-						$query_Scat_insert = "INSERT INTO categories (id,id_parent,name,status) 
-							VALUES (NULL,'{$arr['id']}','{$Scat}',1)";
+						$query_Scat_insert = "INSERT INTO categories (id,id_parent,name,url,status) 
+							VALUES (NULL,'{$arr['id']}','{$Scat}','".slugify($Scat)."',1)";
 						$rst_Scat = mysqli_query($cnt, $query_Scat_insert);
 						$message = '<div class="alert alert-info">Votre sous menu a bien été inséré</div>';
 						echo $message;
@@ -73,7 +73,7 @@ if ( $_POST ) {
 		// condition 1er menu n' existe pas
 		else {
 			// insertion du menu
-			$query_cat_insert = "INSERT INTO `categories` (`id`, `id_parent`, `name`, `status`) VALUES ( NULL,0,'{$_POST['cat']}','1')";
+			$query_cat_insert = "INSERT INTO `categories` (`id`,`id_parent`,`name`,`url`,`status`) VALUES ( NULL,0,'{$_POST['cat']}','".slugify($cat)."',1)";
 			$rst_cat_insert = mysqli_query($cnt, $query_cat_insert);
 			if ( isset($Scat) && strlen($Scat) == 0 ) {
 				$message = '<div class="alert alert-success">Votre menu a bien été inséré</div>';
@@ -93,8 +93,8 @@ if ( $_POST ) {
 					$rst_cat_select = mysqli_query($cnt,$query_cat_select);
 					// creation d un nouveau sous menu
 					while ($arr = mysqli_fetch_array($rst_cat_select)) {
-					$query_Scat_insert = "INSERT INTO categories (id,id_parent,name,status) 
-						VALUES (NULL,'{$arr['id']}','{$Scat}',1)";
+					$query_Scat_insert = "INSERT INTO categories (id,id_parent,name,url,status) 
+						VALUES (NULL,'{$arr['id']}','{$Scat}','".slugify($Scat)."',1)";
 					$rst_Scat = mysqli_query($cnt, $query_Scat_insert);
 					$message = '<div class="alert alert-info">votre sous menu a bien été inséré</div>';
 					echo $message;

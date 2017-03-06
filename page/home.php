1a34ee2277bed2b1599b6a9a6229a40a6fad8d15
gregouns <greglelay@gmail.com>
2017-03-06 18:44:49 +0100

    correction for one line for each post

--- page/home.php
+++ page/home.php
@@ -7,22 +7,29 @@
 		$cnt3 = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
 		mysqli_query($cnt3, "SET NAMES 'utf8'");
 
-		$query = "SELECT * , UNIX_TIMESTAMP(date) AS timestamp FROM posts WHERE status = 1 ORDER BY date DESC LIMIT 10";
+		$query = "SELECT * , UNIX_TIMESTAMP(date) AS timestamp FROM posts 
+			WHERE status = 1 ORDER BY date DESC LIMIT 10";
 		$rst = mysqli_query($cnt, $query);
 		while($arr = mysqli_fetch_array($rst)) {
-			$query2 = "SELECT * ,t.url AS turl FROM tags AS t,posts_tags AS pt WHERE pt.post_id = '{$arr['id']}' AND pt.tag_id = t.id";
-			$rst2 = mysqli_query($cnt2,$query2);
-			while($arr2 = mysqli_fetch_array($rst2)) {
-				echo 'tag: <a href = "/tags/'.$arr2['turl'].'">' . $arr2['tag'] . '</a><br/>';
-			}
-			$query3 = "SELECT * ,c.url AS curl FROM categories AS c,posts_cats AS pc WHERE pc.post_id = '{$arr['id']}' AND pc.cat_id = c.id";
-			$rst3 = mysqli_query($cnt3,$query3);
-			while($arr3 = mysqli_fetch_array($rst3)) {
-				echo 'categorie:<a style="color:green;" href = "/categorie/'.$arr3['curl'].'">' . $arr3['name'] . '</a><br/>';
-			}
-			echo 'Title: <a style="color:orange;" href="/post/'.$arr['url'].'">' . $arr['title'] . '</a><br />';
+			echo 'Title:';
+			echo '<a style="color:orange;" href="/post/'.$arr['url'].'">' . $arr['title'] . '</a><br />';
 			echo 'Date: ' . date('D j M Y', $arr['timestamp']) . '<br/> Description: ' . $arr['description'] . '<br/>';
-			echo '<br/><br/>';
+			$query2 = "SELECT * ,t.url AS turl FROM tags AS t,posts_tags AS pt 
+				WHERE pt.post_id = '{$arr['id']}' AND pt.tag_id = t.id";
+			$rst2 = mysqli_query($cnt2,$query2);
+			
+			echo 'tag: ';
+			while($arr2 = mysqli_fetch_array($rst2)) {
+				echo  '<a href = "/tags/'.$arr2['turl'].'">' . $arr2['tag'] . '</a>,';
+			}
+			$query3 = "SELECT * ,c.url AS curl FROM categories AS c,posts_cats AS pc 
+				WHERE pc.post_id = '{$arr['id']}' AND pc.cat_id = c.id";
+			$rst3 = mysqli_query($cnt3,$query3);
+			echo '<br/>categorie: ';
+			while($arr3 = mysqli_fetch_array($rst3)) {
+				echo '<a style="color:green;" href = "/categorie/'.$arr3['curl'].'">' . $arr3['name'] . '</a>,';
+			}
+			echo '<br/><br/>';	
 		}
 
 ?>

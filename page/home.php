b597c7a72db5a6f127160837b4c58c4050d3e367
gregouns <greglelay@gmail.com>
2017-03-07 12:25:20 +0100

    add line edit post

--- page/home.php
+++ page/home.php
@@ -29,6 +29,7 @@
 			while($arr3 = mysqli_fetch_array($rst3)) {
 				echo '<a style="color:green;" href = "/categorie/'.$arr3['curl'].'">' . $arr3['name'] . '</a>,';
 			}
+			echo '<br/><a href = "/edit/'.$arr['url'].'">modifier votre post</a>';
 			echo '<br/><br/>';
 		}
 

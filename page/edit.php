d4b4abc904087013a6941a24118bef5b4dfea6d4
gregouns <greglelay@gmail.com>
2017-03-08 11:05:35 +0100

    edit page with echo form

--- page/edit.php
+++ page/edit.php
@@ -1,15 +1,5 @@
 <?php
-
-if(isset($POST['title']) && isset($_POST['date']) && isset($_POST['description'])) {
-    $query = "UPDATE posts SET
-            `title` = '{$_POST['title']}',
-            `date` = '{$_POST['date']}',
-            `description` = '{$_POST['description']}'
-        WHERE
-            id = '{$id}'";
-$rst = mysqli_query($cnt, $query);
-}
-
+$message = '';
 $cnt2 = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
 mysqli_query($cnt2, "SET NAMES 'utf8'");
 $cnt3 = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
@@ -17,45 +7,119 @@ mysqli_query($cnt3, "SET NAMES 'utf8'");
 
 $id = $_GET['edit'];
 
-$query = "SELECT *, DATE_FORMAT(date,'%y-%m-%d') FROM posts AS p WHERE id = $id";
-var_dump($query);
-$rst = mysqli_query($cnt, $query);
-while($arr = mysqli_fetch_array($rst)) {
-	var_dump($arr['date']);
-	$query2 = "SELECT GROUP_CONCAT(`tag` SEPARATOR ',') AS `tag` FROM tags AS t, posts_tags AS pt WHERE pt.post_id = '{$arr['id']}' AND t.id = pt.tag_id";
-	$rst2 = mysqli_query($cnt2, $query2);
-	while($arr2 = mysqli_fetch_array($rst2)) {
-		$query3 = "SELECT GROUP_CONCAT(`name` SEPARATOR ',') AS name FROM categories AS c, posts_cats AS pc WHERE pc.post_id = '{$arr['id']}' AND c.id = pc.cat_id";
-		$rst3 = mysqli_query($cnt3, $query3);
-		while($arr3 = mysqli_fetch_array($rst3)) {
-			echo '<form method="post" action="/post">
-					<div class="form-group">
-						<label for="titre">titre</label>
-						<input id="titre" class="form-control" name="title" type="text" value="'.$arr['title'] . '" />
-					</div>
-					<div class="form-group">
-						<label for="date">date</label>
-						<input id="date" class="form-control" name="date" type="datetime" value="'.$arr['date'] . '" />
-					</div>
-					<div class="form-group">
-						<label for="description">description</label>
-						<textarea id="description" class="form-control" name="description">'.$arr['description'] . '</textarea>
-					</div>
-					<div class="form-group">
-						<label for="tag">tag</label>
-						<input id="tag" class="form-control" name="tag" type="text" value="'.$arr2['tag'] . '" />
-					</div>
-					<div class="form-group">
-						<label for="category_parent">catégorie</label>
-						<input id="category_parent" class="form-control" name="category_parent" type="text" value="'.$arr3['name'] . '" />
-					</div>
+function toSELECTEdit ($arr, $pass = 0, $nameSelect = 'category_parent',$arr3 ='' ) {
+  $html = '';
+  if ($pass == 0) {
+    $html = '<select name="'.$nameSelect.'" class="form-control">' . PHP_EOL;
+    $html .= '<option value="-1">' . $arr3 . '</option>';
+  }
+  foreach ( $arr as $v ) {
+    $html .= '<option value="'.$v['id'].'">';
+    $html .= str_repeat("--", $pass); // use the $pass value to create the --
+    $html .= $v['name'] . '</option>' . PHP_EOL;
+    if ( array_key_exists('children', $v) ) {
+      $html .= toSELECT($v['children'], $pass+1);
+    }
+  }
+  if ($pass == 0) $html.= '</select>' . PHP_EOL;
+  return $html;
+}
 
-					<button type="submit" name="submit" id="button" value="submit"  class="btn btn-primary">
-		    			submit
-		  			</button>
-				</form>';
+if (isset($_POST['update'])) {
+	$title = $_POST['title'];
+	$title = addslashes(strip_tags(trim($title)));
+	$date  = $_POST['date'];
+	$description = $_POST['description'];
+	$description = addslashes(strip_tags(trim($description)));
+	$tag = $_POST['tag'];
+	$tag     = addslashes(strip_tags(trim($tag)));
+	$query = "UPDATE posts 
+		SET 
+		title = '{$title}',
+		date  = '{$date}',
+		description = '{$description}'
+		";
+		var_dump($query);
+	$rst = mysqli_query($cnt,$query);
+	$query = "UPDATE tags 
+		SET 
+		tag = '{$tag}'
+		";
+		var_dump($query);
+	$rst = mysqli_query($cnt,$query);
+}
+
+$query = "SELECT *, DATE_FORMAT(date,'%y-%m-%d') FROM posts AS p WHERE id = $id";
+if ($rst = mysqli_query($cnt, $query)) {
+	while($arr = mysqli_fetch_array($rst)) {
+		$query2 = "SELECT GROUP_CONCAT(`tag` SEPARATOR ',') AS `tag` FROM tags AS t, posts_tags AS pt WHERE pt.post_id = '{$arr['id']}' AND t.id = pt.tag_id";
+		$rst2 = mysqli_query($cnt2, $query2);
+		while($arr2 = mysqli_fetch_array($rst2)) {
+			$query3 = "SELECT name FROM categories AS c, posts_cats AS pc WHERE pc.post_id = '{$arr['id']}' AND c.id = pc.cat_id";
+			$rst3 = mysqli_query($cnt3, $query3);
+			while($arr3 = mysqli_fetch_array($rst3)) {
+				$arr_tree = buildTree();
+				$arr['date'] = substr($arr['date'], 0,10);
+				echo   '<form method="post" action="/edit/'.$id.'">
+							<div class="form-group">
+								<label for="titre">titre</label>
+								<input id="titre" class="form-control" name="title" type="text" value="'.$arr['title'] . '" />
+							</div>
+							<div class="form-group">
+								<label for="date">date</label>
+								<input id="date" class="form-control" name="date" type="datetime" value="'.$arr['date'] . '" />
+							</div>
+							<div class="form-group">
+								<label for="description">description</label>
+								<textarea id="description" class="form-control" name="description">'.$arr['description'] . '</textarea>
+							</div>
+							<div class="form-group">
+								<label for="tag">tag</label>
+								<input id="tag" class="form-control" name="tag" type="text" value="'.$arr2['tag'] . '" />
+							</div>
+							<div class="input-group">';
+		    					echo toSELECTEdit($arr_tree, 0, 'category_parent[]' , $arr3['name']);
+								echo '	<span class="input-group-btn">
+									      	<button type="button" class="plus btn btn-success">
+									        	<i class="glyphicon glyphicon-plus"></i>
+									      	</button>
+									      	<button type="button" class="minus btn btn-danger" disabled="disabled">
+									        	<i class="glyphicon glyphicon-minus"></i>
+									      	</button>
+									    </span>
+							</div>
+							<div id="anotherCategory"></div>
+
+							<div class="clearfix"></div>
+							<br />
+							<button type="submit" name="update" id="button" value="submit"  class="btn btn-primary">
+						    submit
+						  	</button>
+						</form>';
+						echo `<script type="text/javascript">
+					  		$(document).ready(function() {
+					   			 $(document).on('click', '.plus', function(e) {
+					      			e.preventDefault();
+					      			var el = $(e.currentTarget);
+					      			var clone = el.parent().parent().clone();
+					      			$('#anotherCategory').append(clone);
+					      			console.log($('#anotherCategory').find('.minus'));
+					      			$('#anotherCategory').find('.minus').prop("disabled", false);
+					   			 });
+					   			 $(document).on('click', '.minus', function(e) {
+					      			e.preventDefault();
+					      			var el = $(e.currentTarget);
+					      			el.parent().parent().remove();
+					   			 });
+					  		});
+					</script>`;
+			}
 		}
 	}
+	echo $message = '<div class="alert alert-success">Votre post a bien été modifié</div>';
+}
+else {
+	echo $message = '<div class="alert alert-danger">Un problème est survenu, veuillez contacter le webmaster</div>';
 }
 ?>
 	

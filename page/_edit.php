b20e148479a71e496adc404edd4f5dd32cf58ef5
gregouns <greglelay@gmail.com>
2017-03-16 01:52:37 +0100

    Modif edit

---
+++ page/_edit.php
@@ -0,0 +1,220 @@
+<?php
+
+$id_edit_post = $_GET['edit'];
+
+if (isset($_POST['submit'])) {
+	// update title, description and date
+	$title = $_POST['title'];
+	$title = addslashes(strip_tags(trim($title)));
+	$date  = $_POST['date'];
+	$description = $_POST['description'];
+	$description = addslashes(strip_tags(trim($description)));
+	$query = "UPDATE posts
+		SET
+		`title` = '{$title}',
+		`date`  = '{$date}',
+		`description` = '{$description}',
+		`url` = '".slugify($title)."'
+		";
+	mysqli_real_escape_string($cnt, $title);
+	$rst = mysqli_query($cnt,$query);
+
+	if(isset($_POST['category_parent'])) {
+		if($_POST['category_parent'] > 0){
+			$arr_cat = $_POST['category_parent'];
+		}
+	}
+	foreach ($arr_cat as $key => $cat_id) {
+		$query = "UPDATE posts_cats
+		SET
+		`cat_id` = '{$cat_id}'
+		WHERE post_id = $id_edit_post";
+		$rst = mysqli_query($cnt,$query);
+	}
+
+	$query = "";
+}
+
+if (isset($_POST)) {
+	// select title description and date
+	$query = "SELECT * FROM posts WHERE id = '{$id_edit_post}'";
+	$rst = mysqli_query($cnt,$query);
+	while ($arr = mysqli_fetch_array($rst)) {
+	$arr_post = array(
+		'titre' => array(
+			'name' => 'title',
+			'type' => 'text',
+			'value' => $arr['title']
+			),
+		'date' => array(
+			'name' => 'date',
+			'type' => 'datetime',
+			'value' => $arr['date']
+			)
+		);
+	$description = $arr['description'];
+	}
+
+	// select categories
+	$query = "SELECT *,pc.post_id AS pcid, p.id AS pid, pc.cat_id AS pccid, c.id AS cid FROM categories AS c, posts_cats AS pc, posts AS p WHERE pc.post_id = '{$id_edit_post}' AND pc.cat_id = c.id";
+	$rst = mysqli_query($cnt,$query);
+	while ($arr = mysqli_fetch_array($rst)) {
+		$name[] = $arr['name'];
+		$cat_id = $arr['pccid'];
+		var_dump($cat_id);
+		$arr_tree = buildTree();
+	}
+}
+function generate($array) {
+	$html = '';
+	foreach ($array as $val) {
+		$html .= '<div class = "form-group">';
+		$html .= '<label>' . $val['name'] . '</label><br/>';
+		$html .= '<input class="form-control" name = "'.$val['name'].'" type = "'.$val['type'].'" value = "'.$val['value'].'" /><br/></div>';
+	}
+
+	return $html;
+}
+
+
+/* ------------------------------------------------------- */
+/* duke */
+/* ------------------------------------------------------- */
+?>
+<script type="text/javascript">
+  function findElementByText(text){
+    // console.log($("div#newtag:contains('"+text+"')"));
+      var jSpot=$("div#newtag:contains('"+text+"')")
+        .filter(function(){ return $(this).children().length === 0;})
+        // .parent();  // because you asked the parent of that element
+
+      return jSpot;
+   }
+  $(document).ready(function() {
+    // Version simple quasiment dupliquée ! Peut etre améliroée !
+    // Mode sale !! mais rapide !
+    $(document).on('click', '.existing_tag', function(e) {
+      e.preventDefault();
+      var el = $(e.currentTarget);
+      var el2 = el.clone();
+      el2.removeClass('existing_tag').addClass('added_tag');
+      el.remove();
+
+      var id = el.attr('data-id');
+      $('#toinsert').val(id+',');
+      $('#newtag').append(el2);
+      $('#newtag').append(' ');
+    });
+    $(document).on('click', '.added_tag', function(e) {
+      var el = $(e.currentTarget);
+      var el2 = el.clone();
+      el2.removeClass('added_tag').addClass('existing_tag');
+      el.remove();
+
+      $('#alltag').append(el2);
+      $('#alltag').append(' ');
+    });
+    $(document).on('click', '.plustag', function(e) {
+      $('#newtag').append('<a href="#" class="added_tag">'+$('#mynewtag').val()+'</a>');
+      $('#newtag').append(' ');
+      $('#mynewtag').val('');
+    });
+  });
+</script>
+<?php
+
+
+
+/* ------------------------------------------------------- */
+/* end */
+/* ------------------------------------------------------- */
+
+
+echo '<form method="post" action="/edit/'.$id_edit_post.'">';
+echo generate($arr_post);
+echo '<div class = "form-group"><label>description</label><br/><textarea class="form-control" name = "description" type = "text">' .$description . ' </textarea><br/></div>';
+$query = "SELECT * FROM tags";
+$rst = mysqli_query($cnt,$query);
+echo '<label for="tag">tag</label><br/><u>Ajouter des tags parmis les tags existants :</u>';
+echo '<div id="alltag">';
+while ($arr = mysqli_fetch_assoc($rst)) {
+  echo '<a href="#" class="existing_tag" data-id="'.$arr['id'].'">'.$arr['tag'].'</a> ';
+  $id = $arr['id'];
+}
+?>
+</div>
+<u>ou ajouter de nouveaux tags :</u>
+<div class="input-group">
+<span class="input-group-btn"><input type="text" id="mynewtag" placeholder="votre tag" class="form-control" />
+      <button type="button" class="plustag btn btn-success">
+        <i class="glyphicon glyphicon-plus"></i>
+      </button></span></div>
+<u>Vos tags sélectionnés:</u>
+<div id="newtag" style="padding: 20px; border:1px solid #ccc; background: #eee;"></div>
+<input id="toinsert" name="newtag" type="hidden" value="" />
+<hr /><br /><br />
+<?php 	
+if(isset($name)){
+ 	foreach ($name as $key => $value) {
+		echo '<div class="input-group">
+				<label for="category">define your categories</label>';
+
+		echo toSELECT($arr_tree, 0, 'category_parent[]',$value);
+		echo '<span class="input-group-btn">
+			<button type="button" class="plus btn btn-success">
+				<i class="glyphicon glyphicon-plus"></i>
+			</button>
+			<button type="button" class="minus btn btn-danger">
+				<i class="glyphicon glyphicon-minus"></i>
+			</button>
+		</span>
+		</div>
+		<div id="anotherCategory"></div>
+
+		<div class="clearfix"></div>
+		<br />';
+	}
+}
+else {
+	echo '<div class="input-group">
+				<label for="category">define your categories</label>';
+		$arr_tree = buildTree();
+		echo toSELECT($arr_tree, 0, 'category_parent[]');
+		echo '<span class="input-group-btn">
+			<button type="button" class="plus btn btn-success">
+				<i class="glyphicon glyphicon-plus"></i>
+			</button>
+			<button type="button" class="minus btn btn-danger">
+				<i class="glyphicon glyphicon-minus"></i>
+			</button>
+		</span>
+		</div>
+		<div id="anotherCategory"></div>
+
+		<div class="clearfix"></div>
+		<br />';
+}
+echo '<button type="submit" name="submit" id="button" value="submit"  class="btn btn-primary">
+		submit
+	</button>';
+echo '</form>';
+?>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$(document).on('click', '.plus', function(e) {
+			e.preventDefault();
+			var el = $(e.currentTarget);
+			var clone = el.parent().parent().clone();
+			$('#anotherCategory').append(clone);
+			console.log($('#anotherCategory').find('.minus'));
+			$('#anotherCategory').find('.minus').prop("disabled", false);
+		});
+		$(document).on('click', '.minus', function(e) {
+			e.preventDefault();
+			var el = $(e.currentTarget);
+			el.parent().parent().remove();
+		});
+	});
+</script>
+
+
--- page/edit.php
+++ page/edit.php
@@ -1,234 +1,336 @@
 <?php
 
 $id_edit_post = $_GET['edit'];
+$arr_tree = buildTree();
 
+
+/**
+ * 1. Charger les données du post
+ * 2. Charger les données des categories associées au posts
+ * 3. Charger les données des tags associées au posts
+ * 4. Afficher les données dans le formulaire
+ * 5. On charge les données du post dans le formulaire
+ * 6. On doit mettre à jour les données du post essentiellement
+ * 	6.1 Je m'assure que je n'ai pas de données corrompues
+ * 	6.2 Je m'assure que je n'ai pas de données vides dans les camps obligatoires (quels sont ils ???)
+ * 	6.3 Je fais en sorte de regénérer un slug de titre (slugify du titre)
+ * 	6.4 En cas d'erreur en 6.1 ou 6.2 je renvoi un message d'erreur
+ * 	6.5 Je renvois un message de félicitation si l'insertion s'est bien réalisée
+ * 7. J'affiche mes tags dans le formulaire
+ * 	7.1 Mes tags existant doivent comporter une action javascript permettant de les retirer et doivent figurer dans le div des tags *
+ * "actuels"
+ *  7.2 Si je retire des tags précédents ajoutés par erreur je dois pouvoir les remettre
+ *  7.3 Ajouter VISUELLEMENT un ou des tags
+ * 		7.3.1 Si le tag est ajouté il est mis dans la liste des tags ajoutés
+ * 		7.3.2 Si le tag est supprimé il doit disparaitre car non présent dans ma base de donnée
+ * 		7.3.3 Si le tag existe déjà il faut alors ne pas l'ajouter mais glisser le tag existant de tous vers les tags ajoutés
+ * 8. J'arriche mes categories dans le formulaire
+ * 	8.1 Je dois duplique le select tant que j'ai des categories
+ * 	8.2 Toutes les categories doivent être supprimables "x" à l'exception de la premiere sinon je fais disparaitre l'ajout de categorie
+ * 9. Ajouter les tags selectionnés dans un input visible puis caché à terme
+ * 10. Mettre les tags ID dans un tableau de type arrUpdateTagIds[] = id
+ * 11. Mettre les tags NAMES dans un tableau de type arrUpdateTagNames[] = name
+ * 
+ **/
+
+/**
+ * 6. On doit mettre à jour les données du post essentiellement
+ * */
 if (isset($_POST['submit'])) {
-	// update title, description and date
-	$title = $_POST['title'];
-	$title = addslashes(strip_tags(trim($title)));
-	$date  = $_POST['date'];
-	$description = $_POST['description'];
-	$description = addslashes(strip_tags(trim($description)));
-	$query = "UPDATE posts
-		SET
-		`title` = '{$title}',
-		`date`  = '{$date}',
-		`description` = '{$description}',
-		`url` = '".slugify($title)."'
-		";
-	mysqli_real_escape_string($cnt, $title);
-	$rst = mysqli_query($cnt,$query);
-	// update tags
- 	$tag = $_POST['tag'];
-	foreach ($tag as $val) {
-		$query = "UPDATE tags
-		SET
-		`tag` = '{$val}',
-		`url` = '".slugify($val)."'
-		";
-		$rst = mysqli_query($cnt,$query);
 
+	print_r($_POST);
+	// 6.1 Je m'assure que je n'ai pas de données corrompues
+	// 6.2 Je m'assure que je n'ai pas de données vides dans les camps obligatoires (quels sont ils ???)
+	$title = $_POST['title'];
+	$title = cleaner($title);
+	var_dump($title);
+	$description = $_POST['description'];
+	$description = cleaner($description);
+	$date = $_POST['date'];
+	$date = cleaner($date);
+
+	// 6.3 Je fais en sorte de regénérer un slug de titre (slugify du titre)
+	$title_url = slugify($title);
+
+	if(true) {
+		echo 'ben oui';
 	}
-	if(isset($_POST['category_parent'])) {
-		if($_POST['category_parent'] > 0){
-			$arr_cat = $_POST['category_parent'];
+	else {
+		echo 'ben non';
+	}
+
+	// 6.4 En cas d'erreur en 6.1 ou 6.2 je renvoi un message d'erreur
+	if ($title != '') {
+		if ($description != '') {
+			if ($date != '') {
+				$query = "UPDATE posts
+					SET
+					`title` = '{$title}',
+					`date`  = '{$date}',
+					`description` = '{$description}',
+					`url` = '$title_url'
+					";
+				if (mysqli_query($cnt, $query)) {
+					// 6.5 Je renvois un message de félicitation si l'insertion s'est bien réalisée
+					echo '<div class="alert alert-success">Votre post a bien été modifié</div>';
+
+					// 10. Mettre les tags ID dans un tableau de type arrUpdateTagIds[] = id
+					$arrUpdateTagIds = explode(',', $_POST['tags_ids']);
+
+					// 11. Mettre les tags NAMES dans un tableau de type arrUpdateTagNames[] = name
+					$arrUpdateTagNames = explode(',', $_POST['tags_names']);
+
+					print_r($arrUpdateTagIds);
+					print_r($arrUpdateTagNames);
+				}
+				else {
+					echo '<div class="alert alert-danger">un problème est survenu , contactez le webmaster</div>';
+				}
+			}
+			else {
+				echo '<div class="alert alert-warning">veuillez rentrer une date valable</div>';
+			}
+		}
+		else{
+			echo '<div class="alert alert-warning">veuillez rentrer une description valable</div>';
 		}
 	}
-	foreach ($arr_cat as $key => $cat_id) {
-		$query = "UPDATE posts_cats
-		SET
-		`cat_id` = '{$cat_id}'
-		WHERE post_id = $id_edit_post";
-		$rst = mysqli_query($cnt,$query);
+	else {
+		echo '<div class="alert alert-warning">veuillez rentrer un titre valable</div>';
 	}
 }
 
-if (isset($_POST)) {
-	// select title description and date
-	$query = "SELECT * FROM posts WHERE id = '{$id_edit_post}'";
-	$rst = mysqli_query($cnt,$query);
-	while ($arr = mysqli_fetch_array($rst)) {
-	$arr_post = array(
-		'titre' => array(
-			'name' => 'title',
-			'type' => 'text',
-			'value' => $arr['title']
-			),
-		'date' => array(
-			'name' => 'date',
-			'type' => 'datetime',
-			'value' => $arr['date']
-			)
-		);
-	$description = $arr['description'];
-	}
-	// select tag
-	$query = "SELECT *,t.id AS tid FROM tags AS t,posts_tags AS pt,posts AS p  WHERE p.id = '{$id_edit_post}' AND pt.post_id = p.id AND pt.tag_id = t.id";
-	$rst = mysqli_query($cnt,$query);
-	while ($arr = mysqli_fetch_assoc($rst)) {
-		$tag[] = $arr['tag'];
-	}
 
-	// select categories
-	$query = "SELECT *,pc.post_id AS pcid, p.id AS pid, pc.cat_id AS pccid, c.id AS cid FROM categories AS c, posts_cats AS pc, posts AS p WHERE pc.post_id = '{$id_edit_post}' AND pc.cat_id = c.id";
-	var_dump($query);
-	$rst = mysqli_query($cnt,$query);
-	while ($arr = mysqli_fetch_array($rst)) {
-		$name[] = $arr['name'];
-		$cat_id = $arr['pccid'];
-		var_dump($cat_id);
-		$arr_tree = buildTree();
-	}
-}
-function generate($array) {
-	$html = '';
-	foreach ($array as $val) {
-		$html .= '<div class = "form-group">';
-		$html .= '<label>' . $val['name'] . '</label><br/>';
-		$html .= '<input class="form-control" name = "'.$val['name'].'" type = "'.$val['type'].'" value = "'.$val['value'].'" /><br/></div>';
-	}
 
-	return $html;
+/**
+ * 1. Charger les données du post
+ * objet ou tableau
+ * $arr->title // $arr['title'] 
+ * 
+ * $arr_post['title'] = $arr['title'];
+ * */
+$query = "SELECT * FROM posts where id = '{$id_edit_post}' LIMIT 1";
+$rst = mysqli_query($cnt, $query);
+$arr_post = mysqli_fetch_array($rst);
+print_r($arr_post);
+
+
+/**
+ * 5. On charge les données du post dans le formulaire
+ * On remplace la valeur de mon post dans $_POST si toutefois il n'existe pas
+ * */
+if (!isset($_POST) || sizeof($_POST) == 0) {
+	$_POST = $arr_post;
 }
 
 
-/* ------------------------------------------------------- */
-/* duke */
-/* ------------------------------------------------------- */
+/**
+ * 2. Charger les données des categories associées au posts
+ * -> $arr_categories[id] = name
+ * */
+$arr_categories = array();
+$query = "SELECT 
+		*
+	FROM 
+		categories AS c, 
+		posts_cats AS pc 
+	WHERE 
+		c.id = pc.cat_id
+		AND pc.post_id = '{$id_edit_post}'
+";
+$rst = mysqli_query($cnt, $query);
+while ($arr = mysqli_fetch_array($rst)) {
+	$arr_categories[$arr['id']] = $arr['name'];
+}
+print_r($arr_categories);
+
+
+/**
+ * 3. Charger les données des tags associées au posts
+ * -> $arr_tags[id] = name
+ * */
+$arr_tags = array();
+$query = "SELECT 
+		*
+	FROM 
+		tags AS t, 
+		posts_tags AS pt 
+	WHERE 
+		t.id = pt.tag_id
+		AND pt.post_id = '{$id_edit_post}'
+";
+$rst = mysqli_query($cnt, $query);
+while ($arr = mysqli_fetch_array($rst)) {
+	$arr_tags[$arr['id']] = $arr['tag'];
+}
+print_r($arr_tags);
+
 ?>
-<script type="text/javascript">
-  function findElementByText(text){
-    // console.log($("div#newtag:contains('"+text+"')"));
-      var jSpot=$("div#newtag:contains('"+text+"')")
-        .filter(function(){ return $(this).children().length === 0;})
-        // .parent();  // because you asked the parent of that element
 
-      return jSpot;
-   }
-  $(document).ready(function() {
-    // Version simple quasiment dupliquée ! Peut etre améliroée !
-    // Mode sale !! mais rapide !
-    $(document).on('click', '.existing_tag', function(e) {
-      e.preventDefault();
-      var el = $(e.currentTarget);
-      var el2 = el.clone();
-      el2.removeClass('existing_tag').addClass('added_tag');
-      el.remove();
-
-      $('#newtag').append(el2);
-      $('#newtag').append(' ');
-    });
-    $(document).on('click', '.added_tag', function(e) {
-      var el = $(e.currentTarget);
-      var el2 = el.clone();
-      el2.removeClass('added_tag').addClass('existing_tag');
-      el.remove();
-
-      $('#alltag').append(el2);
-      $('#alltag').append(' ');
-    });
-    $(document).on('click', '.plustag', function(e) {
-      $('#newtag').append('<a href="#" class="added_tag">'+$('#mynewtag').val()+'</a>');
-      $('#newtag').append(' ');
-      $('#mynewtag').val('');
-    });
-  });
-</script>
-<?php
-
-
-$query = "SELECT * FROM tags";
-$rst = mysqli_query($cnt,$query);
-echo '<hr /><h2>DUKE -- tag</h2><hr /><u>Ajouter des tags parmis les tags existants :</u>';
-echo '<div id="alltag">';
-while ($arr = mysqli_fetch_assoc($rst)) {
-  echo '<a href="#" class="existing_tag" data-id="'.$arr['id'].'">'.$arr['tag'].'</a> ';
-}
-echo '</div>';
-echo '<u>ou ajouter de nouveaux tags :</u>';
-echo '<div class="input-group">
-<span class="input-group-btn"><input type="text" id="mynewtag" placeholder="votre tag" class="form-control" />
-      <button type="button" class="plustag btn btn-success">
-        <i class="glyphicon glyphicon-plus"></i>
-      </button></span></div>';
-echo '<u>Vos tags sélectionnés:</u>';
-echo '<div id="newtag" style="padding: 20px; border:1px solid #ccc; background: #eee;"></div>';
-echo '<hr /><br /><br />';
-/* ------------------------------------------------------- */
-/* end */
-/* ------------------------------------------------------- */
-
-
-echo '<form method="post" action="/edit/'.$id_edit_post.'">';
-echo generate($arr_post);
-echo '<div class = "form-group"><label>description</label><br/><textarea class="form-control" name = "description" type = "text">' .$description . ' </textarea><br/></div>';
-if(isset($tag)){
-	$tag = implode(",", $tag);
-	echo '<div class = "form-group"><label>tag</label><br/><input class="form-control" name = "tag[]" type = "text" value = "'.$tag.'" /><br/></div>';
-}
-else {
-	echo '<div class = "form-group"><label>tag</label><br/><input class="form-control" name = "tag[]" type = "text" value = "" /><br/></div>';
-}
-if(isset($name)){
- 	foreach ($name as $key => $value) {
-		echo '<div class="input-group">
-				<label for="category">define your categories</label>';
-
-		echo toSELECT($arr_tree, 0, 'category_parent[]',$value);
-		echo '<span class="input-group-btn">
-			<button type="button" class="plus btn btn-success">
-				<i class="glyphicon glyphicon-plus"></i>
-			</button>
-			<button type="button" class="minus btn btn-danger">
-				<i class="glyphicon glyphicon-minus"></i>
-			</button>
-		</span>
+<form method="post" action="/edit/<?php echo $id_edit_post ?>">
+	<div class="form-group">
+		<label for="titre">titre</label>
+		<input id="titre" class="form-control" name="title" type="text" value="<?php echo (isset($_POST['title']) ? $_POST['title'] : ''); ?>" />
+	</div>
+	<div class="form-group">
+		<label for="date">date</label>
+		<input id="date" class="form-control" name="date" type="datetime" value="<?php echo (isset($_POST['date']) ? $_POST['date'] : ''); ?>" />
+	</div>
+	<div class="form-group">
+		<label for="description">description</label>
+		<textarea id="description" class="form-control" name="description"><?php echo (isset($_POST['description']) ? $_POST['description'] : ''); ?></textarea>
+	</div>
+	<div class="form-group">
+		<label for="tag">define your tags</label>
+		<div class="input-group">
+			<input id="tag" class="form-control" name="tag" type="text" value="" />
+			<span class="input-group-btn">
+				<button type="button" class="plustag btn btn-success">
+					<i class="glyphicon glyphicon-plus"></i>
+				</button>
+			</span>
 		</div>
-		<div id="anotherCategory"></div>
-
-		<div class="clearfix"></div>
-		<br />';
-	}
-}
-else {
-	echo '<div class="input-group">
-				<label for="category">define your categories</label>';
-		$arr_tree = buildTree();
-		echo toSELECT($arr_tree, 0, 'category_parent[]');
-		echo '<span class="input-group-btn">
-			<button type="button" class="plus btn btn-success">
-				<i class="glyphicon glyphicon-plus"></i>
-			</button>
-			<button type="button" class="minus btn btn-danger">
-				<i class="glyphicon glyphicon-minus"></i>
-			</button>
-		</span>
+	</div>
+	<div class="form-group">
+		<label for="tag">Tags ajoutés</label>
+		<input id="tags_ids" type="text" name="tags_ids" />
+		<input id="tags_names" type="text" name="tags_names" />
+		<div id="newtag" style="padding: 20px; border:1px solid #ccc; background: #eee;">
+		<?php 
+			if (count($arr_tags) > 0) {
+				foreach ($arr_tags as $id => $tag) {
+					echo '<a href="" class="added_tag" data-id="'.$id.'">'.$tag.'</a> ';
+				}
+			}
+		?>
 		</div>
-		<div id="anotherCategory"></div>
+	</div>
+	<div class="form-group">
+		<label for="tag">Tags retirés (tous)</label>
+		<div id="alltags" style="padding: 20px; border:1px solid #eee; background: #f2f2f2;">
+		</div>
+	</div>
 
-		<div class="clearfix"></div>
-		<br />';
-}
-echo '<button type="submit" name="submit" id="button" value="submit"  class="btn btn-primary">
+	<label for="category">define your categories</label>
+ 	
+ 		<?php 
+			if (count($arr_categories) > 0) {
+				$i = 0;
+				foreach ($arr_categories as $id => $cat) {
+					echo '<div class="input-group">';
+						echo toSELECT($arr_tree, 0, 'category_parent[]', $id);
+						?>
+						<span class="input-group-btn">
+							<button type="button" class="plus btn btn-success">
+								<i class="glyphicon glyphicon-plus"></i>
+							</button>
+							<button type="button" class="minus btn btn-danger" <?php echo ( $i === 0 ? 'disabled="disabled"' : '' ); ?>>
+								<i class="glyphicon glyphicon-minus"></i>
+							</button>
+						</span>
+					</div>
+					<?php
+					$i++;
+				}
+			}
+			else {
+				echo '<div class="input-group">';
+					echo toSELECT($arr_tree, 0, 'category_parent[]');
+					?>
+					<span class="input-group-btn">
+						<button type="button" class="plus btn btn-success">
+							<i class="glyphicon glyphicon-plus"></i>
+						</button>
+						<button type="button" class="minus btn btn-danger" disabled="disabled">
+							<i class="glyphicon glyphicon-minus"></i>
+						</button>
+					</span>
+				</div>
+				<?php
+			}
+		?>
+	<div id="anotherCategory"></div>
+
+	<div class="clearfix"></div>
+	<br />
+	<button type="submit" name="submit" id="button" value="submit"  class="btn btn-primary">
 		submit
-	</button>';
-echo '</form>';
-echo `<script type="text/javascript">
+	</button>
+</form>
+
+<script type="text/javascript">
 	$(document).ready(function() {
-		$(document).on('click', '.plus', function(e) {
-			e.preventDefault();
-			var el = $(e.currentTarget);
-			var clone = el.parent().parent().clone();
-			$('#anotherCategory').append(clone);
-			console.log($('#anotherCategory').find('.minus'));
-			$('#anotherCategory').find('.minus').prop("disabled", false);
-		});
-		$(document).on('click', '.minus', function(e) {
-			e.preventDefault();
-			var el = $(e.currentTarget);
-			el.parent().parent().remove();
-		});
-	});
-</script>`;
+	    $(document).on('click', '.added_tag', function(event) {
+	    	event.preventDefault();
+	    	console.log(event);
+      		var el = $(event.currentTarget);
 
+			var clone = el.clone();
+			clone.removeClass('added_tag').addClass('existing_tag');
+			el.remove();
 
+			$('#alltags').append(clone);
+			$('#alltags').append(' ');
+
+			$('#tags_ids').val('');
+			$.each($('#newtag').find('a'), function(key, a) {
+				console.log(a);
+				var id = $(a).attr('data-id');
+				console.log(id)
+				$('#tags_ids').val($('#tags_ids').val()+id+',')
+			});
+	  	});
+	    $(document).on('click', '.existing_tag', function(event) {
+	    	event.preventDefault();
+	    	var el = $(event.currentTarget);
+	    	var id = el.attr('data-id');
+
+	    	if (id > 0) {
+				var clone = el.clone();
+				clone.removeClass('existing_tag').addClass('added_tag');
+				el.remove();
+
+				$('#newtag').append(clone);
+				$('#newtag').append(' ');
+
+				$('#tags_ids').val('');
+				$.each($('#newtag').find('a'), function(key, a) {
+					console.log(a);
+					var id = $(a).attr('data-id');
+					console.log(id)
+					$('#tags_ids').val($('#tags_ids').val()+id+',')
+				});
+			}
+			else {
+				el.remove();
+				$('#tags_names').val('');
+				$.each($('#newtag').find('a'), function(key, a) {
+					var id = $(a).attr('data-id');
+					if (!id) {
+						$('#tags_names').val($('#tags_names').val()+$(a).text()+',')
+					}
+				});
+			}
+	    });
+	    $(document).on('click', '.plustag', function(event) {
+	    	var val = $('#tag').val();
+	    	if (val.length > 0) {
+			$('#newtag').append('<a href="#" class="existing_tag">'+$('#tag').val()+'</a>');
+				$('#newtag').append(' ');
+				$('#tag').val('');
+
+				$('#tags_names').val('');
+				$.each($('#newtag').find('a'), function(key, a) {
+					var id = $(a).attr('data-id');
+					if (!id) {
+						$('#tags_names').val($('#tags_names').val()+$(a).text()+',')
+					}
+				});
+			}
+			else {
+				alert('Please insert tag');
+			}
+		});
+
+  	});
+</script>
